70bebf44c53c87d9dd301fdbd8f6fd08845a9acc
igor-pimenta-araujo <igor.araujo0147@gmail.com>
2022-08-15 17:17:12 -0300

    componetização

---
+++ src/components/AppItemList.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <small> {{ items }} </small>
+    <h5>
+      {{title}}
+      <span class="badge bg-info">{{ items.length }}</span>
+    </h5>
+    <div class="card">
+      <div class="card-body">
+        <ul class="list-group">
+          <li class="list-group-item" v-for="item in items" v-bind:key="item">
+            {{ item }}
+            <div class="col-md text-end">
+              <button class="btn bg-info" v-on:click="deleteItem(item)">
+                <span class="fa fa-trash"></span>
+              </button>
+            </div>
+          </li>
+        </ul>
+        <br />
+        <div class="input-group">
+          <input
+            class="form-control"
+            type="text"
+            v-model="item"
+            v-on:keyup.enter="addItem(item)"
+            placeholder="Digite o item"
+          />
+          <button class="btn btn-info" v-on:click="addItem(item)">
+            <span class="fa fa-plus"></span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppItemList",
+  props: ["title", "items"],
+  data() {
+    return {
+      item: "",
+    };
+  },
+  methods: {
+    addItem (item) {
+      this.$emit("addItem", item);
+      this.item = "";
+    },
+    deleteItem (item) {
+      this.$emit("deleteItem", item);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/DomainList.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <div id="slogan" class="text-center">
+      <h1>NameGator</h1>
+      <br />
+      <h6 class="text-secondary">
+        Gerador de nomes utilizando Vue.js, GraphQL e Node
+      </h6>
+    </div>
+    <div id="main">
+      <div class="container">
+        <div class="row">
+          <div class="col-md">
+          <AppItemList title="Prefixos" v-bind:items="prefixes" v-on:addItem="addPrefix" v-on:deleteItem="deletePrefix"></AppItemList>
+          </div>
+          <div class="col-md">
+          <AppItemList title="Sufixos" v-bind:items="sufixes" v-on:addItem="addSufix" v-on:deleteItem="deleteSufix"></AppItemList>
+          </div>
+        </div>
+        <br />
+        <h5>
+          Domínios <span class="badge bg-info">{{ domains.length }}</span>
+        </h5>
+        <div class="card">
+          <div class="card-body">
+            <ul class="list-group">
+              <li
+                class="list-group-item"
+                v-for="domain in domains"
+                v-bind:key="domain.name">
+					<div class="row">
+						<div class="col-md">{{ domain.name }}</div>
+						<div class="col-md text-end">
+							<a class="btn btn-info" v-bind:href="domain.checkout" target="_blank">
+								<span class="fa fa-shopping-cart"></span>
+							</a>						
+						</div>
+					</div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+import AppItemList from "./AppItemList.vue";
+
+export default {
+	name: "app",
+  components:{
+    AppItemList
+  },
+	data: function () {
+		return {
+			prefixes: ["Air", "Jet", "Flight"],
+			sufixes: ["Hub", "Station", "Mart"]
+		};
+	},
+	methods: {
+		addPrefix: function (prefix) {
+			this.prefixes.push(prefix);
+			this.prefix = "";
+		},
+		addSufix: function (sufix) {
+			this.sufixes.push(sufix);
+			this.sufix = "";
+		},
+		deletePrefix: function (prefix) {
+			this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
+		},
+		deleteSufix: function (sufix) {
+			this.sufixes.splice(this.sufixes.indexOf(sufix), 1);
+		}
+	},
+	computed: {
+		domains() {
+			const domains = [];
+			for (let prefix of this.prefixes) {
+				for (let sufix of this.sufixes) {
+					const name = prefix + sufix; 
+					const checkout = "https://registro.br/busca-dominio/?fqdn=" + name.toLowerCase();
+					domains.push({
+						name, checkout
+					});
+				}
+			}
+			return domains;
+		}
+	}
+};
+</script>
+
+<style>
+#slogan {
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+#main {
+  background-color: #f1f1f1;
+  padding-top: 30px;
+  padding-bottom: 30px;
+}
+</style>
