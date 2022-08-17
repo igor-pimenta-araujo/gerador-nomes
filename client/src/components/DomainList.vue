ec07ad4c0f77d5ab78faa482d76f7968481c6423
igor-pimenta-araujo <igor.araujo0147@gmail.com>
2022-08-17 14:49:08 -0300

    mutation e inputType

--- client/src/components/DomainList.vue
+++ client/src/components/DomainList.vue
@@ -1,133 +1,144 @@
 <template>
-  <div>
-    <div id="slogan" class="text-center">
-      <h1>NameGator</h1>
-      <br />
-      <h6 class="text-secondary">
-        Gerador de nomes utilizando Vue.js, GraphQL e Node
-      </h6>
-    </div>
-    <div id="main">
-      <div class="container">
-        <div class="row">
-          <div class="col-md">
-          <AppItemList title="Prefixos" v-bind:items="prefixes" v-on:addItem="addPrefix" v-on:deleteItem="deletePrefix"></AppItemList>
-          </div>
-          <div class="col-md">
-          <AppItemList title="Sufixos" v-bind:items="sufixes" v-on:addItem="addSufix" v-on:deleteItem="deleteSufix"></AppItemList>
-          </div>
-        </div>
-        <br />
-        <h5>
-          Domínios <span class="badge bg-info">{{ domains.length }}</span>
-        </h5>
-        <div class="card">
-          <div class="card-body">
-            <ul class="list-group">
-              <li
-                class="list-group-item"
-                v-for="domain in domains"
-                v-bind:key="domain.name">
-					<div class="row">
-						<div class="col-md">{{ domain.name }}</div>
-						<div class="col-md text-end">
-							<a class="btn btn-info" v-bind:href="domain.checkout" target="_blank">
-								<span class="fa fa-shopping-cart"></span>
-							</a>						
+	<div class="container">
+		<div class="row">
+			<div class="col-md">
+				<AppItemList title="Prefixos" type="prefix" v-bind:items="items.prefix" v-on:addItem="addItem" v-on:deleteItem="deleteItem"></AppItemList>
+			</div>
+			<div class="col-md">
+				<AppItemList title="Sufixos" type="suffix" v-bind:items="items.suffix" v-on:addItem="addItem" v-on:deleteItem="deleteItem"></AppItemList>
+			</div>
+		</div>
+		<br/>
+		<h5>Domínios <span class="badge badge-info">{{ domains.length }}</span></h5>
+		<div class="card">
+			<div class="card-body">
+				<ul class="list-group">
+					<li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
+						<div class="row">
+							<div class="col-md">
+								{{ domain.name }}
+							</div>
+							<div class="col-md text-end">
+								<a class="btn btn-info" v-bind:href="domain.checkout" target="_blank">
+									<span class="fa fa-shopping-cart"></span>
+								</a>
+							</div>
 						</div>
-					</div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import "bootstrap/dist/css/bootstrap.css";
-import "font-awesome/css/font-awesome.css";
-import AppItemList from "./AppItemList.vue";
-import axios from "axios";
+import axios from "axios/dist/axios";
+import AppItemList from "./AppItemList";
 
 export default {
 	name: "app",
-  components:{
-    AppItemList
-  },
+	components: {
+		AppItemList
+	},
 	data: function () {
 		return {
-			prefixes: [],
-			sufixes: []
+			items: {
+				prefix: [],
+				suffix: []
+			}
 		};
 	},
 	methods: {
-		addPrefix: function (prefix) {
-			this.prefixes.push(prefix);
-			this.prefix = "";
+		addItem(item) {
+			axios({
+				url: "http://localhost:4000",
+				method: "post",
+				data: {
+					query: `
+						mutation ($item: ItemInput) {
+							newItem: saveItem(item: $item) {
+								id
+								type
+								description
+							}
+						}
+					`,
+					variables: {
+						item
+					}
+				}
+			}).then(response => {
+				const query = response.data;
+				const newItem = query.data.newItem;
+				this.items[item.type].push(newItem);
+			});
 		},
-		addSufix: function (sufix) {
-			this.sufixes.push(sufix);
-			this.sufix = "";
+		deleteItem(item) {
+			axios({
+				url: "http://localhost:4000",
+				method: "post",
+				data: {
+					query: `
+						mutation ($id: Int) {
+							deleted: deleteItem(id: $id)
+						}
+					`,
+					variables: {
+						id: item.id
+					}
+				}
+			}).then(() => {
+				this.getItems(item.type);
+			});
 		},
-		deletePrefix: function (prefix) {
-			this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
-		},
-		deleteSufix: function (sufix) {
-			this.sufixes.splice(this.sufixes.indexOf(sufix), 1);
+		getItems(type) {
+			axios({
+				url: "http://localhost:4000",
+				method: "post",
+				data: {
+					query: `
+						query ($type: String) {
+							items: items (type: $type) {
+								id
+								type
+								description
+							}
+						}
+					`,
+					variables: {
+						type
+					}
+				}
+			}).then(response => {
+				const query = response.data;
+				this.items[type] = query.data.items;
+			});
 		}
 	},
 	computed: {
 		domains() {
+			console.log("generating domains...");
 			const domains = [];
-			for (let prefix of this.prefixes) {
-				for (let sufix of this.sufixes) {
-					const name = prefix + sufix; 
-					const checkout = "https://registro.br/busca-dominio/?fqdn=" + name.toLowerCase();
+			for (const prefix of this.items.prefix) {
+				for (const suffix of this.items.suffix) {
+					const name = prefix.description + suffix.description;
+					const url = name.toLowerCase();
+					const checkout = `https://checkout.hostgator.com.br/?a=add&sld=${url}&tld=.com.br`;
 					domains.push({
-						name, checkout
+						name,
+						checkout
 					});
 				}
 			}
 			return domains;
 		}
 	},
-	created(){
-		axios({
-			url: "http://localhost:4000/",
-			method: "post",
-			data: {
-				query: `
-					{
-						prefixes: items(type: "prefix") {
-							id 
-							type
-							description
-						}
-						sufixes: items(type: "sufix") {
-							description
-						}
-					}
-				`
-			}
-		}).then(response => {
-			const query = response.data.data;
-			this.prefixes = query.prefixes.map(prefix => prefix.description);
-			this.sufixes = query.sufixes.map(sufix => sufix.description);
-		});
+	created() {
+		this.getItems("prefix");
+		this.getItems("suffix");
 	}
 };
 </script>
 
 <style>
-#slogan {
-  margin-top: 30px;
-  margin-bottom: 30px;
-}
-#main {
-  background-color: #f1f1f1;
-  padding-top: 30px;
-  padding-bottom: 30px;
-}
 </style>
